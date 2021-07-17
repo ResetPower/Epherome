894c9d3a384911b2397f936543f55362e225d8d2
ResetPower <resetpower20@gmail.com>
2021-07-17 08:34:21 +0800

    fix: build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended", "prettier", "plugin:react-hooks/recommended"],
+  plugins: ["@typescript-eslint", "react-hooks"],
   rules: {
     "@typescript-eslint/no-unused-vars": 0,
     "@typescript-eslint/no-unused-vars-experimental": 1,
