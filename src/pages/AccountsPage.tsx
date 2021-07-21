3b7e4c7b20e4fda796d4a0325fc8ea363f270b6e
ResetPower <resetpower20@gmail.com>
2021-07-21 16:01:45 +0800

    feat: make ui more strong (XD

--- src/pages/AccountsPage.tsx
+++ src/pages/AccountsPage.tsx
@@ -1,27 +1,117 @@
-import { Typography } from "../components/layouts";
-import { Button } from "../components/inputs";
-import { getAccount, MinecraftAccount } from "../struct/accounts";
+import { Alert, Typography } from "../components/layouts";
+import { Button, Select, TextField } from "../components/inputs";
+import {
+  createAccount,
+  CreateAccountImplResult,
+  getAccount,
+  MinecraftAccount,
+} from "../struct/accounts";
 import { logger, t } from "../renderer/global";
 import { ephConfigs, setConfig } from "../renderer/config";
-import { CreateAccountDialog, RemoveAccountDialog } from "../components/Dialogs";
+import { RemoveAccountDialog } from "../components/Dialogs";
 import { MdCreate, MdDelete } from "react-icons/md";
 import { List, ListItem, ListItemText } from "../components/lists";
-import { useForceUpdater } from "../tools/hooks";
+import { useController, useForceUpdater } from "../tools/hooks";
 import GlobalOverlay from "../components/GlobalOverlay";
 import { TabBar, TabBarItem, TabBody, TabController } from "../components/tabs";
 import { useCallback } from "react";
+import { useState } from "react";
+import { DefaultFunction } from "../tools/types";
+import Spin from "../components/Spin";
+
+export function CreateAccountFragment(props: { onDone: DefaultFunction }): JSX.Element {
+  const [errAlert, setErrAlert] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [msAccNoMcAlert, setMsAccNoMcAlert] = useState(false);
+  const valueController = useController("mojang");
+  const authserverController = useController("");
+  const nameController = useController("");
+  const passwordController = useController("");
+
+  const handleCreate = useCallback(() => {
+    setLoading(true);
+    setErrAlert(false);
+    setMsAccNoMcAlert(false);
+    createAccount(
+      valueController.value,
+      nameController.value,
+      passwordController.value,
+      authserverController.value
+    ).then((value: CreateAccountImplResult) => {
+      setLoading(false);
+      if (value.success) {
+        props.onDone();
+      } else {
+        if (value.message === "msAccNoMinecraft") {
+          setMsAccNoMcAlert(true);
+        } else {
+          setErrAlert(true);
+        }
+      }
+    });
+  }, [
+    authserverController.value,
+    nameController.value,
+    passwordController.value,
+    props,
+    valueController.value,
+  ]);
+
+  return (
+    <>
+      <Typography className="text-xl font-semibold">{t.newAccount}</Typography>
+      <div>
+        {errAlert && (
+          <div className="my-3">
+            <Alert severity="warn">{t.errCreatingAccount}</Alert>
+          </div>
+        )}
+        {msAccNoMcAlert && (
+          <div className="my-3">
+            <Alert severity="warn">{t.msAccNoMinecraft}</Alert>
+          </div>
+        )}
+        <Select {...valueController}>
+          <option value={"mojang"}>{t.mojang}</option>
+          <option value={"microsoft"}>{t.microsoft}</option>
+          <option value={"authlib"}>{t.authlib}</option>
+          <option value={"offline"}>{t.offline}</option>
+        </Select>
+        <div hidden={valueController.value !== "mojang"}>
+          <TextField label={t.email} {...nameController} />
+          <TextField label={t.password} {...passwordController} type="password" />
+        </div>
+        <div hidden={valueController.value !== "microsoft"}>
+          <Typography>{t.clickToLogin}</Typography>
+        </div>
+        <div hidden={valueController.value !== "authlib"}>
+          <TextField label={t.authserver} {...authserverController} />
+          <TextField label={t.email} {...nameController} />
+          <TextField label={t.password} {...passwordController} type="password" />
+        </div>
+        <div hidden={valueController.value !== "offline"}>
+          <TextField label={t.username} {...nameController} />
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex-grow">{isLoading && <Spin />}</div>
+        <Button className="text-shallow" onClick={props.onDone} textInherit>
+          {t.cancel}
+        </Button>
+        <Button disabled={isLoading} onClick={handleCreate}>
+          {t.create}
+        </Button>
+      </div>
+    </>
+  );
+}
 
 export default function AccountsPage(): JSX.Element {
   const forceUpdate = useForceUpdater();
   const accounts = ephConfigs.accounts;
   const selected = ephConfigs.selectedAccount;
-  const createAccount = useCallback(
-    () =>
-      GlobalOverlay.showDialog((close) => (
-        <CreateAccountDialog updateAccounts={forceUpdate} onClose={close} />
-      )),
-    [forceUpdate]
-  );
+  const [creating, setCreating] = useState(false);
+  const createAccount = useCallback(() => setCreating(true), []);
   const removeAccount = useCallback(
     () =>
       GlobalOverlay.showDialog((close) => (
@@ -67,29 +157,42 @@ export default function AccountsPage(): JSX.Element {
           ))}
         </List>
       </div>
-      <TabController className="flex-grow p-3" orientation="horizontal">
-        <TabBar>
-          <TabBarItem value={0}>{t.general}</TabBarItem>
-          <TabBarItem value={1}>{t.edit}</TabBarItem>
-        </TabBar>
-        <TabBody>
-          <div className="flex flex-col">
-            <div className="flex-grow">
-              <Typography>ID: {current?.id}</Typography>
-              <Typography>{current?.name}</Typography>
-              <Typography>{current && t[current.mode]}</Typography>
+      {creating ? (
+        <div className="border-l border-divide p-3">
+          <CreateAccountFragment
+            onDone={() => {
+              setCreating(false);
+              forceUpdate();
+            }}
+          />
+        </div>
+      ) : (
+        <TabController className="flex-grow p-3" orientation="horizontal">
+          <TabBar>
+            <TabBarItem value={0}>{t.general}</TabBarItem>
+            <TabBarItem value={1}>{t.edit}</TabBarItem>
+          </TabBar>
+          <TabBody>
+            <div className="flex flex-col">
+              <div className="flex-grow">
+                <Typography className="text-shallow" textInherit>
+                  ID: {current?.id}
+                </Typography>
+                <Typography>{current?.name}</Typography>
+                <Typography>{current && t[current.mode]}</Typography>
+              </div>
+              <div className="flex justify-end">
+                <Button className="text-red-500" onClick={removeAccount} textInherit>
+                  <MdDelete /> {t.remove}
+                </Button>
+              </div>
             </div>
-            <div className="flex justify-end">
-              <Button className="text-red-500" onClick={removeAccount} textInherit>
-                <MdDelete /> {t.remove}
-              </Button>
+            <div>
+              <Typography>{t.notSupportedYet}</Typography>
             </div>
-          </div>
-          <div>
-            <Typography>{t.notSupportedYet}</Typography>
-          </div>
-        </TabBody>
-      </TabController>
+          </TabBody>
+        </TabController>
+      )}
     </div>
   );
 }
--- src/pages/ProfilesPage.tsx
+++ src/pages/ProfilesPage.tsx
@@ -1,34 +1,84 @@
-import { Button } from "../components/inputs";
+import { Button, TextField } from "../components/inputs";
 import { Typography } from "../components/layouts";
-import { getProfile, MinecraftProfile } from "../struct/profiles";
+import { createProfile, editProfile, getProfile, MinecraftProfile } from "../struct/profiles";
 import { hist, logger, t } from "../renderer/global";
 import { ephConfigs, setConfig } from "../renderer/config";
-import { CreateProfileDialog, EditProfileDialog, RemoveProfileDialog } from "../components/Dialogs";
-import { MdCreate, MdEdit, MdFileDownload } from "react-icons/md";
+import { RemoveProfileDialog } from "../components/Dialogs";
+import { MdCreate, MdFileDownload, MdFolder } from "react-icons/md";
 import { List, ListItem, ListItemText } from "../components/lists";
-import { useForceUpdater } from "../tools/hooks";
+import { useController, useForceUpdater } from "../tools/hooks";
 import GlobalOverlay from "../components/GlobalOverlay";
 import { TabBar, TabBarItem, TabBody, TabController } from "../components/tabs";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import { ipcRenderer } from "electron";
+import { DefaultFunction } from "../tools/types";
+import { useEffect } from "react";
+import fs from "fs";
+import path from "path";
+
+export function ChangeProfileFragment(props: {
+  onDone: DefaultFunction;
+  action: "edit" | "create";
+  current?: MinecraftProfile;
+}): JSX.Element {
+  const current = props.current;
+  const nameController = useController(current?.name ?? "");
+  const dirController = useController(current?.dir ?? "");
+  const verController = useController(current?.ver ?? "");
+
+  const handleEdit = () => {
+    if (current) {
+      editProfile(current.id, nameController.value, dirController.value, verController.value);
+      props.onDone();
+    }
+  };
+  const handleCreate = () => {
+    if (createProfile(nameController.value, dirController.value, verController.value)) {
+      props.onDone();
+    }
+  };
+  const handleOpenDirectory = () => {
+    ipcRenderer.once("replyOpenDirectory", (_ev, arg) => {
+      dirController.onChange(arg);
+    });
+    ipcRenderer.send("openDirectory");
+  };
+  return (
+    <>
+      {props.action === "create" && (
+        <Typography className="text-xl font-semibold">{t.newProfile}</Typography>
+      )}
+      <div>
+        <TextField label={t.name} {...nameController} />
+        <TextField label={t.directory} {...dirController} helperText={t.usuallyDotMinecraftEtc} />
+        <TextField label={t.version} {...verController} />
+      </div>
+      <div className="flex">
+        <Button onClick={handleOpenDirectory}>
+          <MdFolder /> {t.openDirectory}
+        </Button>
+        <div className="flex-grow"></div>
+        <Button className="text-shallow" onClick={props.onDone}>
+          {t.cancel}
+        </Button>
+        {props.action === "create" ? (
+          <Button onClick={handleCreate}>{t.create}</Button>
+        ) : (
+          <Button onClick={handleEdit}>{t.edit}</Button>
+        )}
+      </div>
+    </>
+  );
+}
 
 export default function ProfilesPage(): JSX.Element {
   const forceUpdate = useForceUpdater();
   const profiles = ephConfigs.profiles;
   const selected = ephConfigs.selectedProfile;
-  const createProfile = useCallback(
-    () =>
-      GlobalOverlay.showDialog((close) => (
-        <CreateProfileDialog onClose={close} updateProfiles={forceUpdate} />
-      )),
-    [forceUpdate]
-  );
-  const editProfile = useCallback(
-    () =>
-      GlobalOverlay.showDialog((close) => (
-        <EditProfileDialog onClose={close} updateProfiles={forceUpdate} id={selected} />
-      )),
-    [forceUpdate, selected]
-  );
+  const [mapsList, setMapsList] = useState<string[]>([]);
+  const [resourcePacksList, setResourcePacksList] = useState<string[]>([]);
+  const [creating, setCreating] = useState(false);
+  const createProfile = () => setCreating(true);
   const removeProfile = useCallback(
     () =>
       GlobalOverlay.showDialog((close) => (
@@ -37,6 +87,21 @@ export default function ProfilesPage(): JSX.Element {
     [forceUpdate, selected]
   );
   const current = getProfile(selected);
+
+  useEffect(() => {
+    if (current) {
+      fs.readdir(path.join(current.dir, "saves"), (err, files) => {
+        if (!err) {
+          setMapsList(files);
+        }
+      });
+      fs.readdir(path.join(current.dir, "resourcepacks"), (err, files) => {
+        if (!err) {
+          setResourcePacksList(files);
+        }
+      });
+    }
+  }, [current]);
 
   return profiles.length === 0 ? (
     <div className="flex flex-col eph-h-full justify-center items-center">
@@ -75,42 +140,72 @@ export default function ProfilesPage(): JSX.Element {
           ))}
         </List>
       </div>
-      <TabController className="flex-grow p-3" orientation="horizontal">
-        <TabBar>
-          <TabBarItem value={0}>{t.general}</TabBarItem>
-          <TabBarItem value={1}>{t.edit}</TabBarItem>
-          <TabBarItem value={2}>{t.maps}</TabBarItem>
-          <TabBarItem value={3}>{t.resourcePacks}</TabBarItem>
-        </TabBar>
-        <TabBody>
-          <div className="flex flex-col">
-            <div className="flex-grow">
-              <Typography>ID: {current?.id}</Typography>
-              <Typography>
-                {t.name}: {current?.name}
-              </Typography>
-              <Typography>
-                {t.directory}: {current?.dir}
-              </Typography>
-              <Typography>
-                {t.version}: {current?.ver}
-              </Typography>
+      {creating ? (
+        <div className="border-l border-divide p-3">
+          <ChangeProfileFragment
+            action="create"
+            onDone={() => {
+              setCreating(false);
+              forceUpdate();
+            }}
+          />
+        </div>
+      ) : (
+        <TabController className="flex-grow p-3" orientation="horizontal">
+          <TabBar>
+            <TabBarItem value={0}>{t.general}</TabBarItem>
+            <TabBarItem value={1}>{t.edit}</TabBarItem>
+            <TabBarItem value={2}>{t.maps}</TabBarItem>
+            <TabBarItem value={3}>{t.resourcePacks}</TabBarItem>
+          </TabBar>
+          <TabBody>
+            <div className="flex flex-col">
+              <div className="flex-grow">
+                <Typography>ID: {current?.id}</Typography>
+                <Typography>
+                  {t.name}: {current?.name}
+                </Typography>
+                <Typography>
+                  {t.directory}: {current?.dir}
+                </Typography>
+                <Typography>
+                  {t.version}: {current?.ver}
+                </Typography>
+              </div>
+              <div className="flex justify-end">
+                <Button className="text-red-500" onClick={removeProfile} textInherit>
+                  {t.remove}
+                </Button>
+              </div>
             </div>
-            <div className="flex justify-end">
-              <Button className="text-red-500" onClick={removeProfile} textInherit>
-                {t.remove}
-              </Button>
+            <div>
+              {current && (
+                <ChangeProfileFragment action="edit" onDone={forceUpdate} current={current} />
+              )}
             </div>
-          </div>
-          <div>
-            <Typography>
-              <Button variant="contained" onClick={editProfile}>
-                <MdEdit /> {t.edit}
-              </Button>
-            </Typography>
-          </div>
-        </TabBody>
-      </TabController>
+            <div>
+              {mapsList.map(
+                (m, index) =>
+                  m !== ".DS_Store" && (
+                    /* avoid useless .DS_Store file on macOS */ <Typography key={index}>
+                      {m}
+                    </Typography>
+                  )
+              )}
+            </div>
+            <div>
+              {resourcePacksList.map(
+                (m, index) =>
+                  m !== ".DS_Store" && (
+                    /* avoid useless .DS_Store file on macOS */ <Typography key={index}>
+                      {m}
+                    </Typography>
+                  )
+              )}
+            </div>
+          </TabBody>
+        </TabController>
+      )}
     </div>
   );
 }
